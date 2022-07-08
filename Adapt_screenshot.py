e94e95652df543e7827e10f7a425439d171fe42e
ekazh3 <Ekaterina.Zhukova@steor.tech>
2022-07-08 13:38:15 +0300

    meaningless fixes

--- Adapt_screenshot.py
+++ Adapt_screenshot.py
@@ -1,35 +1,29 @@
 # Требуется скрипт, который мог бы перекрашивать пикселы скриншота формы, лежащие вне замкнутых контуров,
 # ограничивающих контролы, расположенные на этой форме.
-#
-# Скрипт получает на вход
-# 1. или png файл, скриншота, или соответствующий ему двумерный массив,
-# 2. координаты точки, с которой его следут начиеать обходить (по умолчанию: 0, 0 (то есть, верхний левый угол),
-# 3. подмешиваемый цвет в виде одной из трёх строковых констант: "RED", "GREEN" или "BLUE".
-#
-# Если на вход получен png файл, то он скрипт преобразует его в двумерный массив.
-# Скрипт обходит массив, начиная с указанной точки, и строит список связанных с ней точек, того же (или близкого) цвета.
-#
-# После того как список точек для перекраски будет готов, скрипт создаёт копию двумерного массива,
-# в котором точки из сформированного списка будут перкрашены.
-#
-# Опционально новый двумерный массив преобразуется в новый png файл.
-#
-# Алгоритм обхода соседних пикселов находится здесь.
-#
-# Алгоритм преобразования из png в массив и обратно можно уточнить у Стаса.
 
 
 from numpy import asarray
+from numpy import in1d
 from PIL import Image
 
-img = Image.open("image.png")
+img = Image.open("image.png").convert('RGB')
 array_of_pixels = asarray(img)
-
+# print(array_of_pixels)
 RED = [255, 0, 0]
 start_point = (0, 0)
 
 array_of_coords = []
 neighbours = [start_point]
+
+row_from_array = array_of_pixels[1]
+# print(row_from_array)
+neighbours_colors = []
+while len(neighbours_colors) < 4:
+    for nc in row_from_array:
+        if not in1d(nc, neighbours_colors).any():
+            neighbours_colors.append(nc)
+            print(nc)
+print(neighbours_colors)
 
 while neighbours:
     p = neighbours.pop()
@@ -40,15 +34,18 @@ while neighbours:
 
     if n1[0] < len(array_of_pixels):
         if list(array_of_pixels[n1[0]][n1[1]]) == list(array_of_pixels[p[0]][p[1]]):
-            neighbours.append(n1)
+            if n1 not in array_of_coords:
+                neighbours.append(n1)
     if n2[1] < len(array_of_pixels[0]):
         if list(array_of_pixels[n2[0]][n2[1]]) == list(array_of_pixels[p[0]][p[1]]):
-            neighbours.append(n2)
+            if n2 not in array_of_coords:
+                neighbours.append(n2)
+# print(array_of_coords)
 
 new_array_of_pixels = array_of_pixels.copy()
 
 for coord in array_of_coords:
     new_array_of_pixels[coord[0]][coord[1]] = RED
-
+# print(new_array_of_pixels)
 changed_image = Image.fromarray(new_array_of_pixels)
 changed_image.show()
