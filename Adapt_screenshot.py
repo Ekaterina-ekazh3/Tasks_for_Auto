356a8df7f2dded2102ed73ee5198205e78d99dce
ekazh3 <Ekaterina.Zhukova@steor.tech>
2022-08-01 12:24:07 +0300

    change func

--- Adapt_screenshot.py
+++ Adapt_screenshot.py
@@ -1,59 +1,56 @@
-# Требуется скрипт, который мог бы перекрашивать пикселы скриншота формы, лежащие вне замкнутых контуров,
-# ограничивающих контролы, расположенные на этой форме.
-
-
-from numpy import asarray
-from numpy import array_equal
-from numpy import ndarray
+from numpy import asarray, ndarray, array_equal
 from PIL import Image
 
 img = Image.open("time.png").convert('RGB')
 array_of_pixels = asarray(img)
 
-RED = [255, 0, 0]
+height, width = array_of_pixels.shape[:-1:]
+
 start_point = (0, 0)
 
-
-def find_neighbours(neigh: list, arr_of_co: set) -> set:
-    while neigh:
-        p = neigh.pop()
-        arr_of_co.add(p)
-
-        neigh_by_vert = (p[0] + 1, p[1])  # сосед по вертикали
-        neigh_by_hor = (p[0], p[1] + 1)  # сосед по горизонтали
-
-        if neigh_by_vert[0] < len(array_of_pixels):
-            if array_equal(array_of_pixels[neigh_by_vert[0]][neigh_by_vert[1]], array_of_pixels[p[0]][p[1]]):
-                if neigh_by_vert not in arr_of_co:
-                    neigh.append(neigh_by_vert)
-        if neigh_by_hor[1] < len(array_of_pixels[0]):
-            if array_equal(array_of_pixels[neigh_by_hor[0]][neigh_by_hor[1]], array_of_pixels[p[0]][p[1]]):
-                if neigh_by_hor not in arr_of_co:
-                    neigh.append(neigh_by_hor)
-    return arr_of_co
+base_color = array_of_pixels[start_point[0]][start_point[1]]
+target_color = [255, 0, 0]
 
 
-def define_coords(start_pnt):
-    neighbours = [start_pnt]
-    arr_of_coords = set()
-    arr_of_coords = find_neighbours(neighbours, arr_of_coords)
-    return arr_of_coords
+def adapt_screenshot(strt_point, trgt_color, check_func):
+    neighbours = [strt_point]
+    array_of_coords = set()
+    check_function = check_func
+
+    while neighbours:
+        p = neighbours.pop()
+        array_of_coords.add(p)
+
+        neigh_bottom = (p[0] + 1, p[1])  # сосед снизу
+        neigh_right = (p[0], p[1] + 1)  # сосед справа
+        neigh_top = (p[0] - 1, p[1])  # сосед сверху
+        neigh_left = (p[0], p[1] - 1)  # сосед слева
+
+        if neigh_bottom[0] < len(array_of_pixels):
+            if check_function(array_of_pixels[neigh_bottom[0]][neigh_bottom[1]], array_of_pixels[p[0]][p[1]]):
+                if neigh_bottom not in array_of_coords:
+                    neighbours.append(neigh_bottom)
+        if neigh_right[1] < len(array_of_pixels[0]):
+            if check_function(array_of_pixels[neigh_right[0]][neigh_right[1]], array_of_pixels[p[0]][p[1]]):
+                if neigh_right not in array_of_coords:
+                    neighbours.append(neigh_right)
+        if neigh_top[0] >= 0:
+            if check_function(array_of_pixels[neigh_top[0]][neigh_top[1]], array_of_pixels[p[0]][p[1]]):
+                if neigh_top not in array_of_coords:
+                    neighbours.append(neigh_top)
+        if neigh_left[1] >= 0:
+            if check_function(array_of_pixels[neigh_left[0]][neigh_left[1]], array_of_pixels[p[0]][p[1]]):
+                if neigh_left not in array_of_coords:
+                    neighbours.append(neigh_left)
+
+    col_array = array_of_pixels.copy()
+    for coord in array_of_coords:
+        col_array[coord[0]][coord[1]] = trgt_color
+
+    return col_array
 
 
-array_of_coords = define_coords(start_point)
+colored_array = adapt_screenshot(start_point, target_color, array_equal)
 
-new_array_of_pixels = array_of_pixels.copy()
-
-
-def set_colour(colour: list, new_arr: ndarray, arr_co: set) -> list:
-    inner_array_of_coords = arr_co
-    inner_new_array_of_pixels = new_arr
-    for coord in inner_array_of_coords:
-        inner_new_array_of_pixels[coord[0]][coord[1]] = colour
-    return new_array_of_pixels
-
-
-new_array_of_pixels = set_colour(RED, new_array_of_pixels, array_of_coords)
-
-changed_image = Image.fromarray(new_array_of_pixels)
+changed_image = Image.fromarray(colored_array)
 changed_image.show()
